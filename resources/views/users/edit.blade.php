e91735015f36acc1b8d5abd03db8470b1a051541
David Duarte <102606398+daviddmd@users.noreply.github.com>
2022-04-26 21:25:39 +0100

    Edit User Views

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -93,9 +93,6 @@
                                     {{__("Name")}}
                                 </th>
                                 <th scope="col" class="px-6 py-3">
-                                    {{__("Description")}}
-                                </th>
-                                <th scope="col" class="px-6 py-3">
                                     {{__("Type")}}
                                 </th>
                                 <th scope="col" class="px-6 py-3">
@@ -123,7 +120,6 @@
                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                     <td class="px-6 py-4">{{$asset->id}}</td>
                                     <td class="px-6 py-4">{{$asset->name}}</td>
-                                    <td class="px-6 py-4">{{$asset->description}}</td>
                                     <td class="px-6 py-4">{{$asset->type->name}}</td>
                                     <td class="px-6 py-4">{{$asset->sku}}</td>
                                     <td class="px-6 py-4">{{$asset->ip_address}}</td>
@@ -131,7 +127,7 @@
                                     <td class="px-6 py-4">{{$asset->manufacturer}}</td>
                                     <td class="px-6 py-4">{{$asset->location}}</td>
                                     <td class="px-6 py-4">
-                                        @can("update",$control)
+                                        @can("update",$asset)
                                             <a href="{{route("assets.edit",$asset->id)}}"
                                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                                 {{__("Manage")}}
