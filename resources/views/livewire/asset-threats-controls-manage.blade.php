82ccb16038aba723dcce851cabe74382567cd6b6
David Duarte <102606398+daviddmd@users.noreply.github.com>
2022-07-14 20:17:35 +0100

    Fix asset threats control management header

--- resources/views/livewire/asset-threats-controls-manage.blade.php
+++ resources/views/livewire/asset-threats-controls-manage.blade.php
@@ -1,21 +1,22 @@
 <div>
     <h2 class="text-center text-2xl font-normal leading-normal mt-0 mb-2">{{__("Threats")}}</h2>
-    <h3 class="text-center text-2xl font-normal leading-normal mt-0 mb-2 text-red-600 font-bold">{{__("Define the Asset Appreciation Before Adding Threats")}}</h3>
-    <div class="flex justify-center">
-        @if($asset->totalAppreciation()==0)
+    @if($asset->totalAppreciation()==0)
+        <h3 class="text-center text-2xl font-normal leading-normal mt-0 mb-2 text-red-600 font-bold">{{__("Define the Asset Appreciation Before Adding Threats")}}</h3>
+        <div class="flex justify-center">
             <button type="button"
                     class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                 {{__("Add Threat")}}
             </button>
-        @else
+        </div>
+    @else
+        <div class="flex justify-center">
             <button type="button"
                     wire:click="openCreateThreatDialog"
                     class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                 {{__("Add Threat")}}
             </button>
-        @endif
-
-    </div>
+        </div>
+    @endif
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg mb-5">
         @foreach($threats as $threat)
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 border-separate">
